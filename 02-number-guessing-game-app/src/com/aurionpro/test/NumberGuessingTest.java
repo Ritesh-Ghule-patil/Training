ad00eb2fd1aec173c3c20580c65a8a7363073f31
Ritesh Ghule (Patil) <86919789+Ritesh-Ghule-patil@users.noreply.github.com>
2022-07-12 09:20:15 +0530

    Number Guessing Game

---
+++ 02-number-guessing-game-app/src/com/aurionpro/test/NumberGuessingTest.java
@@ -0,0 +1,9 @@
+package com.aurionpro.test;
+
+import com.aurionpro.model.NumberGuessingGame;
+
+public class NumberGuessingTest {
+	public static void main(String[] args) {
+		NumberGuessingGame game = new NumberGuessingGame();
+	}
+}
