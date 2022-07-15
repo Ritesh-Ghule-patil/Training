5f0b3bde952c2de07c268d373fd07ecf6cda2265
Ritesh Ghule (Patil) <86919789+Ritesh-Ghule-patil@users.noreply.github.com>
2022-07-15 19:03:54 +0530

    Reactangle  final

---
+++ 07-rectangle-final/src/com/aurionpro/main/RectangleTest.java
@@ -0,0 +1,29 @@
+package com.aurionpro.main;
+
+import com.aurionpro.model.ColorType;
+import com.aurionpro.model.Rectangle;
+
+public class RectangleTest {
+
+	public static void main(String[] args) {
+		Rectangle smallRectangle = new Rectangle(-2, 101, ColorType.BLUE);
+		printObjectDetail(smallRectangle);
+
+		System.out.println("-----------------------------------------");
+
+		Rectangle bigRectangle = new Rectangle(-200, 20, ColorType.GREEN);
+		printObjectDetail(bigRectangle);
+		
+		
+		
+		
+	}
+
+	public static void printObjectDetail(Rectangle rectangle) {
+		System.out.println("bigRectangle height : " + rectangle.getHeight());
+		System.out.println("bigRectangle witdh : " + rectangle.getWidth());
+		System.out.println("color of rectangle is : " + rectangle.getColor());
+		System.out.println("Area of Big Rectangle is : " + rectangle.calculateArea());
+	}
+
+}
