c906e9ce6f85396b171493f818b7e7e05731e62a
Ritesh Ghule (Patil) <86919789+Ritesh-Ghule-patil@users.noreply.github.com>
2022-07-16 10:31:10 +0530

    Static use

---
+++ 08-static-method-app/src/com/aurionpro/employee/Employee.java
@@ -0,0 +1,18 @@
+package com.aurionpro.employee;
+
+public class Employee {
+	
+	int empId;
+	String name;
+	long salary;
+	
+	public static void main(String[] args) {
+		printDetail();
+	
+	}
+	
+	public static void printDetail() {
+		
+		
+	}
+}
