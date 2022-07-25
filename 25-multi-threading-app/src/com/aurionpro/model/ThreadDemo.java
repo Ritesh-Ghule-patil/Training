65a1994d3e43d5e5162ccfbc2c7e87a5f0956bcc
Ritesh Ghule (Patil) <86919789+Ritesh-Ghule-patil@users.noreply.github.com>
2022-07-25 09:05:42 +0530

    Multi threading app

---
+++ 25-multi-threading-app/src/com/aurionpro/model/ThreadDemo.java
@@ -0,0 +1,11 @@
+package com.aurionpro.model;
+
+public class ThreadDemo extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println("Thead is created");
+	}
+	
+	
+}
---
+++ 25-multi-threading-app/src/com/aurionpro/test/ThreadDemoTest.java
@@ -0,0 +1,11 @@
+package com.aurionpro.test;
+
+import com.aurionpro.model.ThreadDemo;
+
+public class ThreadDemoTest {
+	public static void main(String[] args) {
+		
+		ThreadDemo thread = new ThreadDemo();
+		thread.start();
+	}
+}
